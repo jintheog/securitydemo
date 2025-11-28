19284d2c3ba98b5971369b852effe43f9df1320b
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-28 10:30:06 +0900

    sample user data

--- src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +33,23 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("USER","ADMIN")
+                .build();
+        // new User("user", "{noop}1234, "USER")
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
