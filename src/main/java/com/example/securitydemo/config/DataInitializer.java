aac380c94db28ccc5ce3d60729a1aaecbbe678e2
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-28 14:40:29 +0900

    DataInitializer

--- src/main/java/com/example/securitydemo/config/DataInitializer.java
+++ src/main/java/com/example/securitydemo/config/DataInitializer.java
@@ -1,5 +1,6 @@
 package com.example.securitydemo.config;
 
+import com.example.securitydemo.entity.User;
 import com.example.securitydemo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +15,21 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        User user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("1234"))
+                .role("ROLE_USER")
+                .build();
 
+        User admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .role("ROLE_ADMIN")
+                .build();
+
+        userRepository.save(user);
+        userRepository.save(admin);
+
+        System.out.println("user 데이터 생성완료");
     }
 }
